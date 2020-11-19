6c79f36f815533b91a883aaa9e98894e3016c4b1
Aditya0311 <aditya.398agar@gmail.com>
2020-11-19 12:18:18 +0530

    webdeverror

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -6,7 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT_SUCCESS,
+ 
  
 } from './types';
 //import { IAuthFunction, IConfigHeaders } from '../../types/interfaces';
@@ -33,6 +33,41 @@ export const loadUser = () => (dispatch, getState) => {
       });
     });
 };
+
+export const login = ({ email, password }) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  // Request body
+  const body = JSON.stringify({ email, password });
+
+  axios
+    .post('/api/auth/users', body, config)
+    .then(res =>
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL')
+      );
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
+};
+
+
+
+
+
+
 
 //Setup config/headers and token
 export const tokenConfig = getState => {
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,94 +1,116 @@
-import React, { Component } from 'react';
-import '../../App.css';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import React, { useState, useEffect, useCallback } from "react";
+import "../../App.css";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 //import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { connect } from 'react-redux';
-import { login } from '../../actions/authActions';
-import { clearErrors } from '../../actions/errorActions';
-import PropTypes from 'prop-types';
-import {Alert} from 'reactstrap';
-import { ReactComponent } from '*.svg';
-
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { connect } from "react-redux";
+import { login } from "../../actions/authActions";
+import { clearErrors } from "../../actions/errorActions";
+import { Alert } from "reactstrap";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
- 
+const SignIn = ({
+  isAuthenticated,
+  error,
+  login,
+  clearErrors
+}) => {
 
 //export default function SignIn() {
-class SignIn extends Component {
-  state = {
-    name: '',
-    email:'',
-    password:'',
-    msg: null
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const handleToggle = useCallback(() => {
+    // Clear errors
+    clearErrors();
+  }, [clearErrors]);
+
+  const handleChangeEmail = (e) => setEmail(e.target.value);
+  const handleChangePassword = (e) => setPassword(e.target.value);
+
+
+  const handleOnSubmit = (e) => {
+
+    // e.preventDefault();
+    //console.log(email, password);
+
+    const user = { email, password };
+    console.log("fun calling",user)
+
+    // Attempt to login
+    login(user);
   };
 
-  useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
-  static propTypes = {
+  useEffect(() => {
+    // Check for register error
+    if (error.id === 'LOGIN_FAIL') {
+      setMsg(error.msg.msg);
+    } else {
+      setMsg(null);
+    }
 
-    isAuthenticated: PropTypes.bool,
-    error: PropTypes.object.isRequired,
-    login: PropTypes.func.isRequired,
-    clearErrors: PropTypes.func.isRequired
-  };
+    if (isAuthenticated) {
+      handleToggle();
+    }
+  }, [error, handleToggle, isAuthenticated]);
 
+  
 
-  const classes = this.useStyles(); 
- 
-
-  render() {
+  function onSubmit() { }
+  const classes = useStyles();
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-        </Avatar>
+        <Avatar className={classes.avatar}></Avatar>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        { this.state.msg ? (<Alert color='danger'>{this.state.msg}</Alert>): null}
-        <form className={classes.form} noValidate onSubmit={this.onSubmit}>
+        {msg ? <Alert color="danger">{msg}</Alert> : null}
+        <form className={classes.form} noValidate onSubmit={onSubmit}>
           <TextField
             variant="outlined"
             margin="normal"
@@ -99,6 +121,7 @@ class SignIn extends Component {
             name="email"
             autoComplete="email"
             autoFocus
+            onChange={handleChangeEmail}
           />
           <TextField
             variant="outlined"
@@ -110,13 +133,14 @@ class SignIn extends Component {
             type="password"
             id="password"
             autoComplete="current-password"
+            onChange={handleChangePassword}
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
           />
           <Button
-            //onClick={ }
+            onClick={ handleOnSubmit }
             type="submit"
             fullWidth
             variant="contained"
@@ -125,8 +149,8 @@ class SignIn extends Component {
           >
             Sign In
           </Button>
+
           <Grid container>
-            
             <Grid item>
               <Link href="#" variant="body2">
                 {"Don't have an account? Sign Up"}
@@ -139,7 +163,21 @@ class SignIn extends Component {
         <Copyright />
       </Box>
     </Container>
-  )};
+  );
 }
 
-export default SignIn;
+// SignIn.propTypes = {
+//   isAuthenticated: PropTypes.bool,
+//   error: PropTypes.object.isRequired,
+//   login: PropTypes.func.isRequired,
+//   clearErrors: PropTypes.func.isRequired
+//   };
+
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error
+});
+
+export default connect(mapStateToProps, { login, clearErrors })(SignIn);
+//export default SignIn;
